d77aaa2c9466af8eb72d6160975c226caf680788
Danang Galuh Tegar Prasetyo <connect@danang.id>
2020-06-20 09:28:24 +0700

    better docs

--- init.go
+++ init.go
@@ -1,10 +1,8 @@
-/**
- * Copyright © 2020 The With-Go Authors. All rights reserved.
- *
- * Licensed under the BSD 3-Clause License.
- * You may not use this file except in compliance with the license
- * that can be found in the LICENSE.md file.
- */
+// Copyright © 2020 The With-Go Authors. All rights reserved.
+// Licensed under the BSD 3-Clause License.
+// You may not use this file except in compliance with the license
+// that can be found in the LICENSE.md file.
+
 package config
 
 import (
