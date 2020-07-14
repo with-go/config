6bab4e897fe29b8a4748c2ece4404bc1c6ac3577
Danang Galuh Tegar Prasetyo <connect@danang.id>
2020-07-15 06:36:24 +0700

    Add functions chaining ability on module

--- module.go
+++ module.go
@@ -16,29 +16,30 @@ func NewModule(name string) *Module {
 
 // Type Module defines a named modular configuration.
 //
-// Property Name defines the Module's Name as string.
-//
-// Property Config defines the key-value pair of configuration list as map string
-// of string.
+//  Property Name defines the Module's Name as string.
+//  Property Config defines the key-value pair of configuration list as map string
+//  of string.
 type Module struct {
 	Name	string
 	Config	map[string]string
 }
 
 // Delete module configuration with specified key.
-func (m *Module) Delete(key string) {
-	delete(m.Config, key)
+// Returns the Module for function call chaining purpose.
+func (module *Module) Delete(key string) *Module {
+	delete(module.Config, key)
+	return module
 }
 
 // Get module configuration with specified key.
 // Returns the string value of the module configuration. When no configuration is
 // saved with that specified key, it will returns an empty string.
-func (m *Module) Get(key string) string {
-	value := os.Getenv(strings.ToUpper(m.Name) + "_" + strings.ToUpper(key))
+func (module *Module) Get(key string) string {
+	value := os.Getenv(strings.ToUpper(module.Name) + "_" + strings.ToUpper(key))
 	if value != "" {
 		return value
 	}
-	for k, v := range m.Config {
+	for k, v := range module.Config {
 		if k == key {
 			return v
 		}
@@ -48,8 +49,8 @@ func (m *Module) Get(key string) string {
 
 // The same as Get(key string), but it will returns defaultValue instead of empty
 // string when no configuration is saved with that specified key.
-func (m *Module) GetWithDefault(key string, defaultValue string) string {
-	value := m.Get(key)
+func (module *Module) GetWithDefault(key string, defaultValue string) string {
+	value := module.Get(key)
 	if value != "" {
 		return value
 	}
@@ -57,6 +58,8 @@ func (m *Module) GetWithDefault(key string, defaultValue string) string {
 }
 
 // Set module configuration with specified key to a specified value.
-func (m *Module) Set(key string, value string) {
-	m.Config[key] = value
+// Returns the Module for function call chaining purpose.
+func (module *Module) Set(key string, value string) *Module {
+	module.Config[key] = value
+	return module
 }
